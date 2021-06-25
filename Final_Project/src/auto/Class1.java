e8b87e088f6ac2151e693b863a0f0acb015e7efd
preeto0001 <86212565+preeto0001@users.noreply.github.com>
2021-06-24 23:28:15 -0400

    Add files via upload

---
+++ Final_Project/src/auto/Class1.java
@@ -0,0 +1,20 @@
+package auto;
+import org.openqa.selenium.By;
+	import org.openqa.selenium.WebElement;
+	import org.openqa.selenium.chrome.ChromeDriver;
+	import org.testng.Assert;
+	import org.testng.annotations.Test; class Automated {
+
+	    @Test
+	    public void Harpreet(){
+	        System.setProperty("webdriver.chrome.driver", "C:\\Users\\localadmin\\Downloads\\chromedriver_win32\\chromedriver.exe");
+	        ChromeDriver driver = new ChromeDriver();
+	        driver.get("https://www.google.com");
+	        String actualTitle = "Google";
+	        Assert.assertEquals(driver.getTitle(), actualTitle );
+	       WebElement logo = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/img")  );
+	      Assert.assertEquals(true, logo.isDisplayed());
+	        System.out.println("pass");
+	        driver.close();
+	}
+	}
